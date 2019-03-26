bf0a38c6d50910b9071548f005484de49ccf581e
Bangbert <RobertKirchner1@gmx.de>
2019-03-26 18:11:44 +0100

    Inital Solution Setup

---
+++ RobertsShop/RobertsShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RobertsShop.WebUI.MvcApplication" Language="C#" %>
---
+++ RobertsShop/RobertsShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RobertsShop.WebUI.Startup))]
+namespace RobertsShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
