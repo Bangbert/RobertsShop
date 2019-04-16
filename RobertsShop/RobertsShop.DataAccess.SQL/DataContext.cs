c437cd416d4108a76c7be2ec0d4151182f4d67e3
Bangbert <RobertKirchner1@gmx.de>
2019-04-16 21:03:47 +0200

    Migration Scripting in DataContext for SQL

---
+++ RobertsShop/RobertsShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using RobertsShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobertsShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //Konstruktor bestimmt im Aufruf der Elternklasse Connectionsstring mit Name in Config
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+        // Block to set Models to Tables -> Migration scripting
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories {get; set;}
+        
+
+    }
+}
