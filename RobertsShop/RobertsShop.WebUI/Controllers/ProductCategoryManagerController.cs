964f2094f9d98d6e1c07dcd774082fd16c553e03
Bangbert <RobertKirchner1@gmx.de>
2019-04-16 18:40:49 +0200

    InMemoryRepository implimented as generic to othe Repos

--- RobertsShop/RobertsShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ RobertsShop/RobertsShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -10,12 +10,12 @@ namespace RobertsShop.WebUI.Controllers
 {
     public class ProductCategoryManagerController : Controller
     {
-        ProductCategoryRepository context;
+        InMemoryRepository<ProductCategory> context;
 
 
         public ProductCategoryManagerController()
         {
-            context = new ProductCategoryRepository();
+            context = new InMemoryRepository<ProductCategory>();
 
         }
 
