1c1e3b79fc29db1cc83f2ce15255d754c393161d
Bangbert <RobertKirchner1@gmx.de>
2019-04-29 21:58:46 +0200

    New Basket model added + SQL Database

--- RobertsShop/RobertsShop.DataAccess.SQL/DataContext.cs
+++ RobertsShop/RobertsShop.DataAccess.SQL/DataContext.cs
@@ -18,6 +18,8 @@ namespace RobertsShop.DataAccess.SQL
         // Block to set Models to Tables -> Migration scripting etc
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories {get; set;}
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
         
 
     }
