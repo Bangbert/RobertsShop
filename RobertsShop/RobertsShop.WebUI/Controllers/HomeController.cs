0763d40bd56ed8eb669bb70b16316180f373fc06
Bangbert <RobertKirchner1@gmx.de>
2019-04-29 21:36:35 +0200

    category filtering implimented

--- RobertsShop/RobertsShop.WebUI/Controllers/HomeController.cs
+++ RobertsShop/RobertsShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using RobertsShop.Core.Contracts;
 using RobertsShop.Core.Models;
+using RobertsShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,10 +24,29 @@ namespace RobertsShop.WebUI.Controllers
         }
 
 
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+                products =  context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+
+            return View(model);
+
+
+
         }
         public ActionResult Details(string Id)
         {
